3344795c58dd12692e481c734eef87363eeb4c9e
y47355 <ye.qi@huawei.com>
2026-09-10 19:38:12 +0800

    add core/design/build.gradle.kts

---
+++ core/design/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.spotpal.core.design"
+    compileSdk = 34
+    defaultConfig { minSdk = 26 }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+    buildFeatures { compose = true }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.material3)
+    testImplementation(libs.junit)
+}
