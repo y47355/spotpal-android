ba4a5ddf13d63dc23ba39f04422d04bb8f27e326
y47355 <ye.qi@huawei.com>
2026-09-10 19:37:52 +0800

    add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// 顶层构建文件：模块声明与公共插件版本统一收口。
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ksp) apply false
+}
