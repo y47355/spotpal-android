98932e20649dd5775f511ce183830cf603d452e2
y47355 <ye.qi@huawei.com>
2026-09-10 19:37:55 +0800

    add core/common/build.gradle.kts

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.spotpal.core.common"
+    compileSdk = 34
+    defaultConfig { minSdk = 26 }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    testImplementation(libs.junit)
+}
